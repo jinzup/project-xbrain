94d0be9997cff817415b137f04c37d2da9e86a03
Gabriel Silva <jinsgt@gmail.com>
2022-08-17 20:19:51 -0300

    Project Created

---
+++ src/test/java/com/xbrain/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xbrain.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
