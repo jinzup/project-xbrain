2557dda5e55de6904c178393d3ea3f04c3788c13
Gabriel Silva <jinsgt@gmail.com>
2022-08-17 21:39:55 -0300

    H2 database, test profile, JPA

--- src/main/java/com/xbrain/project/entities/User.java
+++ src/main/java/com/xbrain/project/entities/User.java
@@ -1,13 +1,18 @@
 package com.xbrain.project.entities;
 
+import javax.persistence.*;
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name="users")
 public class User implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
