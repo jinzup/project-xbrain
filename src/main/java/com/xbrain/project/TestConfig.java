053a9931936f90c34d4a2e5cff4864e96126a228
Gabriel Silva <jinsgt@gmail.com>
2022-08-19 06:20:17 -0300

    Category

--- src/main/java/com/xbrain/project/TestConfig.java
+++ src/main/java/com/xbrain/project/TestConfig.java
@@ -1,8 +1,10 @@
 package com.xbrain.project;
 
+import com.xbrain.project.entities.Category;
 import com.xbrain.project.entities.Order;
 import com.xbrain.project.entities.User;
 import com.xbrain.project.entities.enums.OrderStatus;
+import com.xbrain.project.repositories.CategoryRepository;
 import com.xbrain.project.repositories.OrderRepository;
 import com.xbrain.project.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,9 +25,19 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
 
     @Override
     public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
