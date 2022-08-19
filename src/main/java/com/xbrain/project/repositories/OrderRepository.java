da42a9a50f20175dfe0f4b0666022f43100892e9
Gabriel Silva <jinsgt@gmail.com>
2022-08-18 22:54:56 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/xbrain/project/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.xbrain.project.repositories;
+
+import com.xbrain.project.entities.Order;
+import com.xbrain.project.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
